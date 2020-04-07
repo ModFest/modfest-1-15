58d2b61b5bd54d333a83a41a7ddc0338be091fa7
Meredith Espinosa <goclonefilms@gmail.com>
2020-04-06 21:17:38 -0700

    start updating, remove a few things that shouldn't have been in

--- packs/modfest.voodoo.kts
+++ packs/modfest.voodoo.kts
@@ -5,17 +5,17 @@ modloader { fabric(Fabric.intermediary.v_1_15_2) }
 
 root<Curse> {
   it.list {
-    +ProjectID(373595) //gravestones
+    +FabricMod.gravestones
     +FabricMod.funkyForcefields
-    //TODO: azuma goes here when CF approves
-//    +ProjectID(373653) //mechanized TODO: move back here once CF propagates, added below
-    +ProjectID(373591) //yung's better mineshafts
+    +FabricMod.azuma
+    +FabricMod.mechanizedSteamPower
+    +FabricMod.yungsBetterMineshaftsFabric
     +ProjectID(373581) //unfortunately
     +FabricMod.underpowered //TODO: TR crash
     +FabricMod.cornflower
     +FabricMod.composing
-    +FabricMod.campanion //TODO: mixin crash
-//    +FabricMod.lint //TODO: mixin fix propagation, added below
+    +FabricMod.campanion
+    +FabricMod.lint
     +FabricMod.esther
     +ProjectID(373504) //tenor
     +FabricMod.dihydrogenMonoxideReloaded
@@ -44,10 +44,10 @@ root<Curse> {
     }.list {
       +"extraDetectors" {
         url = "https://github.com/ExtraCrafTX/ExtraDetectors/releases/download/v0.0.1/extradetectors-0.0.1.jar"
-        description = "Adds a weather detector similar to a daylight sensor"
+        description = "Adds a weather detector similar to a clock"
       }
       +"vantacine" {
-        url = "https://cdn.discordapp.com/attachments/692799982058864650/696851421672374272/vantacine-1.0.0-modfest.jar"
+        url = "https://cdn.discordapp.com/attachments/692799982058864650/696924186861633617/vantacine-1.0.0.jar"
         description = "Improves the Nether through miscellaneous additions that bring together utility, decoration, and fun."
       }
       +"offShorePlus" {
@@ -75,31 +75,18 @@ root<Curse> {
         description = "Adds magic blocks to repair iron tools"
       }
       +"crimson" {
-        url = "https://github.com/valoeghese/Crimson/releases/download/v1.0.2/crimson-1.0.2.jar"
+        url = "https://github.com/valoeghese/Crimson/releases/download/v1.0.3/crimson-1.0.3.jar"
         description = "Adds a dangerous new crimson biome linked to a system of crafting magic devices from imbued gemstones"
       }
       +"kevlar" {
         url = "https://github.com/Earthcomputer/Kevlar/releases/download/v1.0/kevlar-1.0-SNAPSHOT.jar"
         description = "Early to mid game tech mod"
       }
-      +"mechanized" {//TODO: remove once beta is out
-        url = "https://github.com/Snakefangox/Mechanized/releases/download/modfest/Mechanized-1.0.0.jar"
-      }
       "simpleaccessories" { //TODO: remove once beta is out
         url = "https://github.com/rjmunhoz/simple-accessories/releases/download/v0.1.3/simpleaccessories-0.1.3.jar"
       }
       "netweight" { //TODO: remove once beta is out
         url = "https://github.com/UpcraftLP/Net-Weight/releases/download/0.1.0-alpha%2Bmodfest/net-weight-0.1.0-alpha+custom-1.15.jar"
-      }
-      +"azuma" { //TODO: curse version when it's out
-        url = "https://github.com/TeamSuperPeople/azuma/releases/download/1.2.0-beta-1.15.2/azuma-1.2.0.jar"
-        description = "Our mod adds a special crafting system powered by a magical energy alongside several useful utility items and tools."
-      }
-      +"lint" { //TODO: crash fix
-        url = "https://cdn.discordapp.com/attachments/696868182635315312/696870819460153364/lint-1.1.0.jar"
-      }
-      +"tacocraft" { //TODO: curseforge fingerprint issue
-        url = "https://github.com/franiscoder/TacoCraft/releases/download/1.0.0/tacocraft-1.0.0.jar"
       }
       "sodium" { //TODO: curseforge
         url = "https://cdn.discordapp.com/attachments/690311875287580743/696873450878533702/sodium-mc1.15.2-fabric-0.1.0.jar"
@@ -108,7 +95,7 @@ root<Curse> {
         url = "https://immibisdotcom.s3.amazonaws.com/mcmoddl/modfest2/compuglobalhypermeganet-SayNoToBeesYesToSylladices-1.0.0.jar"
         description = "Makes it painful to use your inventory"
       }
-      "metalSupply" {
+      +"metalSupply" {
         url = "https://github.com/Reoseah/metal-supply/releases/download/modfest/metal-supply-1.0.0.jar"
         description = "Adds Crank and Grinder inspired by Applied Energistics' Grindstone, and 5 new metals: lead, tin, copper, silver and bronze."
       }
